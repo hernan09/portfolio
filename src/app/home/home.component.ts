5386dc35925e356448893a93763501e18123b92f
hernan <hernag_09@hotmail.com>
2019-07-22 17:59:46 -0300

    links + estilos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,22 @@ import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
- 
+
+  links = [
+    {"link":"https://github.com/hernan09/React-App-ShowVideo","name":"Wow Videos","frame":"React.js"},
+    {"link":"https://github.com/hernan09/ionic-animal","name":"sonidos de animales","frame":"Ionic"},
+    {"link":"https://github.com/hernan09/vanila-static","name":"videojuego","frame":"JS Vanilla"},
+    {"link":"https://github.com/hernan09/CDT-app","name":"pagina de noticias","frame":"React.js"},
+    {"link":"https://github.com/hernan09/MapZero","name":"App con Google map","frame":"Angular && Ionic"}
+    
+
+     ]
+  
 
   show:Boolean = false
 
   constructor() { 
+      this.links
     
    }
   
