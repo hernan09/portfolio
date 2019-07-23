374375bf3988b91f31acb6cdb8fce8370d7acdef
hernan <hernag_09@hotmail.com>
2019-07-23 17:51:17 -0300

    animacion de scroll y responsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import * as smoothscroll from "smoothscroll-polyfill";
+import * as $ from 'jquery';
 
 
 
@@ -9,21 +11,20 @@ import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  jquery:any
   links = [
     {"link":"https://github.com/hernan09/React-App-ShowVideo","name":"Wow Videos","frame":"React.js"},
     {"link":"https://github.com/hernan09/ionic-animal","name":"sonidos de animales","frame":"Ionic"},
     {"link":"https://github.com/hernan09/vanila-static","name":"videojuego","frame":"JS Vanilla"},
     {"link":"https://github.com/hernan09/CDT-app","name":"pagina de noticias","frame":"React.js"},
     {"link":"https://github.com/hernan09/MapZero","name":"App con Google map","frame":"Angular && Ionic"}
-    
-
      ]
   
 
   show:Boolean = false
 
   constructor() { 
+       smoothscroll.polyfill();
       this.links
     
    }
@@ -40,8 +41,21 @@ export class HomeComponent implements OnInit {
     }
   }  
   
-  
+  scrollToElement(destino): void {
+   
+    destino.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
 
+  }
+  
+  clickAfuera(){
+    $(document).on('click','.navbar-collapse.in',(e)=> {
+      console.log('corre el jquery')
+      if( $(e.target).is('a:not(".dropdown-toggle")') ) {
+          $(this).collapse('hide');
+      }
+  });
+  }
+  
   ngOnInit() {
   }
 
